72e2e3538c7afaa6816554fc631d873c763eb4e2
cwyu57 <cwyu57@gmail.com>
2021-02-19 13:32:06 +0800

    cdk bootstrap

--- bin/cdk-cloudmap.ts
+++ bin/cdk-cloudmap.ts
@@ -4,4 +4,9 @@ import * as cdk from '@aws-cdk/core';
 import { CdkCloudmapStack } from '../lib/cdk-cloudmap-stack';
 
 const app = new cdk.App();
-new CdkCloudmapStack(app, 'CdkCloudmapStack');
+new CdkCloudmapStack(app, 'CdkCloudmapStack', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+});
